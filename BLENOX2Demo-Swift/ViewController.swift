ae4cf3c73991334d275e2cafb0815ee30a4e9023
Shawsan@sleepace.net <Shawsan@sleepace.net>
2019-09-11 16:47:33 +0800

    Initial Commit

---
+++ BLENOX2Demo-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLENOX2Demo-Swift
+//
+//  Created by San on 11/9/2019.
+//  Copyright © 2019 medica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BLENOX2Demo-SwiftTests/BLENOX2Demo_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  BLENOX2Demo_SwiftTests.swift
+//  BLENOX2Demo-SwiftTests
+//
+//  Created by San on 11/9/2019.
+//  Copyright © 2019 medica. All rights reserved.
+//
+
+import XCTest
+@testable import BLENOX2Demo_Swift
+
+class BLENOX2Demo_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
