378137ea6acdc70449ba8fffbadaa7eba8c25f8a
Shawsan@sleepace.net <Shawsan@sleepace.net>
2019-09-20 17:46:31 +0800

    更新文档接口！

---
+++ Framework/BleNox.framework/Headers/BleNoxLogInfo.h
@@ -0,0 +1,14 @@
+//
+//  BleNoxLogInfo.h
+//  SDK
+//
+//  Created by Martin on 2018/3/2.
+//  Copyright © 2018年 Martin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BleNoxLogInfo : NSObject
+@property (nonatomic, assign) BOOL finished;
+@property (nonatomic, copy) NSString *log;
+@end
